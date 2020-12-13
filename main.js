ce19c5c579911bfa2a433b2c94f8c17591fb7646
Marlon Lavanda <marlonlavanda@hotmail.com>
2020-12-13 14:13:00 +0000

    Filalización del juego

--- main.js
+++ main.js
@@ -3,17 +3,20 @@ const violeta = document.getElementById("violeta");
 const naranja = document.getElementById("naranja");
 const verde = document.getElementById("verde");
 const btnEmpezar = document.getElementById("btnEmpezar");
+const ULTIMO_NIVEL = 10;
 
 class Juego {
   constructor() {
+    this.inicializar = this.inicializar.bind(this);
     this.inicializar();
     this.generarSecuencia();
-    this.siguienteNivel();
+    setTimeout(this.siguienteNivel, 500);
   }
 
   inicializar() {
+    this.siguienteNivel = this.siguienteNivel.bind(this);
     this.elegirColor = this.elegirColor.bind(this);
-    btnEmpezar.classList.add("hide");
+    this.togglebtnEmpezar();
     this.nivel = 1;
     this.colores = {
       celeste,
@@ -23,13 +26,22 @@ class Juego {
     };
   }
 
+  togglebtnEmpezar() {
+    if (btnEmpezar.classList.contains("hide")) {
+      btnEmpezar.classList.remove("hide");
+    } else {
+      btnEmpezar.classList.add("hide");
+    }
+  }
+
   generarSecuencia() {
-    this.secuencia = new Array(10)
+    this.secuencia = new Array(ULTIMO_NIVEL)
       .fill(0)
       .map((n) => Math.floor(Math.random() * 4));
   }
 
   siguienteNivel() {
+    this.subnivel = 0;
     this.iluminarSecuencia();
     this.agregarEventosClick();
   }
@@ -44,6 +56,19 @@ class Juego {
         return "naranja";
       case 3:
         return "verde";
+    }
+  }
+
+  transformarColorANumero(color) {
+    switch (color) {
+      case "celeste":
+        return 0;
+      case "violeta":
+        return 1;
+      case "naranja":
+        return 2;
+      case "verde":
+        return 3;
     }
   }
 
@@ -68,8 +93,44 @@ class Juego {
     this.colores.violeta.addEventListener("click", this.elegirColor);
     this.colores.naranja.addEventListener("click", this.elegirColor);
   }
+
+  eliminarEventosClick() {
+    this.colores.celeste.removeEventListener("click", this.elegirColor);
+    this.colores.verde.removeEventListener("click", this.elegirColor);
+    this.colores.violeta.removeEventListener("click", this.elegirColor);
+    this.colores.naranja.removeEventListener("click", this.elegirColor);
+  }
+
   elegirColor(ev) {
-    console.log(this);
+    const nombreColor = ev.target.dataset.color;
+    const numeroColor = this.transformarColorANumero(nombreColor);
+    this.iluminarColor(nombreColor);
+    if (numeroColor === this.secuencia[this.subnivel]) {
+      this.subnivel++;
+      if (this.subnivel === this.nivel) {
+        this.nivel++;
+        this.eliminarEventosClick();
+        if (this.nivel == ULTIMO_NIVEL + 1) {
+          this.ganoElJuego();
+        } else {
+          setTimeout(this.siguienteNivel, 1500);
+        }
+      }
+    } else {
+      this.perdioElJuego();
+    }
+  }
+  ganoElJuego() {
+    swal("Platzi", "Felicidades, ganaste el juego!", "success").then(
+      this.inicializar
+    );
+  }
+
+  perdioElJuego() {
+    swal("Platzi", "Lo siento, perdiste :(", "error").then(() => {
+      this.eliminarEventosClick();
+      this.inicializar();
+    });
   }
 }
 
